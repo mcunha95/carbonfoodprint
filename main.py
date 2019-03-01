651c88fb8061da77cd98eb53715a54ba12b68c5d
Eric Massip <ericmassip1@gmail.com>
2019-03-01 01:55:57 +0100

    Missing app server in main.py

--- main.py
+++ main.py
@@ -184,6 +184,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 # Bootstrap CSS
 app.css.append_css({'external_url': 'https://codepen.io/amyoshino/pen/jzXypZ.css'})
 
